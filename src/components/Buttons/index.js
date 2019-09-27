bdfc39805c9e275ed8b24f1d1a45e3c377f2c01d
aehaq <azfarehaq@gmail.com>
2019-09-27 10:22:54 -0700

    relabeled buttons and gave them data-ids

--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -3,25 +3,25 @@ import "./style.css";
 
 const Buttons = props => (
     <div className="card-body">
-        <button className="btn btn-primary" id={1} onClick={(e) => props.handleClick(e)}>1</button>
-        <button className="btn btn-primary" id={2} onClick={(e) => props.handleClick(e)}>2</button>
-        <button className="btn btn-primary" id={3} onClick={(e) => props.handleClick(e)}>3</button>
-        <button className="btn btn-danger" id={"+"} onClick={(e) => props.handleClick(e)}>+</button>
+        <button className="btn btn-primary" data-type={"number"} id={"1"} onClick={(e) => props.handleClick(e)}>1</button>
+        <button className="btn btn-primary" data-type={"number"} id={"2"} onClick={(e) => props.handleClick(e)}>2</button>
+        <button className="btn btn-primary" data-type={"number"} id={"3"} onClick={(e) => props.handleClick(e)}>3</button>
+        <button className="btn btn-danger" data-type={"operator"} id={"+"} onClick={(e) => props.handleClick(e)}>+</button>
         <br />
-        <button className="btn btn-primary" id={4} onClick={(e) => props.handleClick(e)}>4</button>
-        <button className="btn btn-primary" id={5} onClick={(e) => props.handleClick(e)}>5</button>
-        <button className="btn btn-primary" id={6} onClick={(e) => props.handleClick(e)}>6</button>
-        <button className="btn btn-danger" id={"-"} onClick={(e) => props.handleClick(e)}>-</button>
+        <button className="btn btn-primary" data-type={"number"} id={"4"} onClick={(e) => props.handleClick(e)}>4</button>
+        <button className="btn btn-primary" data-type={"number"} id={"5"} onClick={(e) => props.handleClick(e)}>5</button>
+        <button className="btn btn-primary" data-type={"number"} id={"6"} onClick={(e) => props.handleClick(e)}>6</button>
+        <button className="btn btn-danger" data-type={"operator"} id={"-"} onClick={(e) => props.handleClick(e)}>-</button>
         <br />
-        <button className="btn btn-primary" id={7} onClick={(e) => props.handleClick(e)}>7</button>
-        <button className="btn btn-primary" id={8} onClick={(e) => props.handleClick(e)}>8</button>
-        <button className="btn btn-primary"id={9} onClick={(e) => props.handleClick(e)}>9</button>
-        <button className="btn btn-danger" id={"x"} onClick={(e) => props.handleClick(e)}>x</button>
+        <button className="btn btn-primary" data-type={"number"} id={"7"} onClick={(e) => props.handleClick(e)}>7</button>
+        <button className="btn btn-primary" data-type={"number"} id={"8"} onClick={(e) => props.handleClick(e)}>8</button>
+        <button className="btn btn-primary" data-type={"number"} id={"9"} onClick={(e) => props.handleClick(e)}>9</button>
+        <button className="btn btn-danger" data-type={"operator"} id={"x"} onClick={(e) => props.handleClick(e)}>x</button>
         <br />
-        <button className="btn btn-primary" id={0} onClick={(e) => props.handleClick(e)}>0</button>
-        <button className="btn btn-danger" id={"/"} onClick={(e) => props.handleClick(e)}>/</button>
-        <button className="btn btn-danger" id={"^"} onClick={(e) => props.handleClick(e)}>^</button>
-        <button className="btn btn-success" id={"="} onClick={(e) => props.handleClick(e)}>=</button>
+        <button className="btn btn-primary" data-type={"number"} id={"0"} onClick={(e) => props.handleClick(e)}>0</button>
+        <button className="btn btn-danger" data-type={"operator"} id={"/"} onClick={(e) => props.handleClick(e)}>/</button>
+        <button className="btn btn-danger" data-type={"operator"} id={"^"} onClick={(e) => props.handleClick(e)}>^</button>
+        <button className="btn btn-success" data-type={"equals"} id={"="} onClick={(e) => props.handleClick(e)}>=</button>
         <br />
         <button className="btn btn-dark">clear</button>
     </div>
